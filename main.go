f8d9e6ff9c23610f5833d29daa11bb989d33b878
MedvedevYuriy <syberiayoubear@gmail.com>
2019-01-24 00:59:05 +0700

    prometheus mertics added in main.go

--- main.go
+++ main.go
@@ -1,45 +1,70 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus/promauto"
+        "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
+var (
+	goapp_requests_count = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "goapp_requests_count",
+			Help: "Number of requests served",
+		},
+		[]string{"type"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(goapp_requests_count)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-  keys, ok := r.URL.Query()["a"]
-  if !ok || len(keys[0]) < 1 {
-      log.Println("Url Param 'a' is missing")
-      return
-  }
+	keys, ok := r.URL.Query()["a"]
+	if !ok || len(keys[0]) < 1 {
+		goapp_requests_count.With(prometheus.Labels{"type": "missinga"}).Inc()
+		log.Println("Url Param 'a' is missing")
+		return
+	}
 
-  a, err := strconv.Atoi(keys[0])
+	a, err := strconv.Atoi(keys[0])
 
-  if err != nil {
-    return
-  }
+	if err != nil {
+		goapp_requests_count.With(prometheus.Labels{"type": "bada"}).Inc()
+		return
+	}
 
-  keys, ok = r.URL.Query()["b"]
-  if !ok || len(keys[0]) < 1 {
-      log.Println("Url Param 'b' is missing")
-      return
-  }
-  b, err := strconv.Atoi(keys[0])
-  if err != nil {
-    log.Println("Bad number!")
-    return
-  }
+	keys, ok = r.URL.Query()["b"]
+	if !ok || len(keys[0]) < 1 {
+		goapp_requests_count.With(prometheus.Labels{"type": "missingb"}).Inc()
+		log.Println("Url Param 'b' is missing")
+		return
+	}
+	b, err := strconv.Atoi(keys[0])
+	if err != nil {
+		goapp_requests_count.With(prometheus.Labels{"type": "badb"}).Inc()
+		log.Println("Url Param 'b' is missing")
+		log.Println("Bad number!")
+		return
+	}
 
-  fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a,a))
+	goapp_requests_count.With(prometheus.Labels{"type": "ok"}).Inc()
+	fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a, a))
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func sum (a, b int) int {
-  t := a + b
-  return t
+func sum(a, b int) int {
+	t := a + b
+	return t
 }
